30a8db3297611dde2a67b24fba5885904abd797a
Konrad Bochenek <56956303+Kazan1520@users.noreply.github.com>
2023-03-01 18:28:26 +0100

    handle python code execution

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import base64
+from contextlib import redirect_stdout
+from io import StringIO
+
 from fastapi import FastAPI
 from pydantic.main import BaseModel
 
@@ -11,8 +15,23 @@ class Request(BaseModel):
     def __str__(self):
         return self.code
 
-
 @app.post("/execute")
 async def say_hello(request: Request):
-    print(request)
-    return request
+    kod = decode_base64(request.code)
+
+    result = StringIO()
+    with redirect_stdout(result):
+        exec(kod)
+    result = result.getvalue()
+    result = result.replace("\n", "")
+    return {"result": result}
+
+
+def decode_base64(code):
+    return base64.b64decode(code).decode('utf-8')
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
