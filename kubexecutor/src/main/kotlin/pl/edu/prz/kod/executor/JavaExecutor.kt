827c24acbf7bddf71759f6bc5b309e3cce1c1f50
Adrian Jurek <57730065+Adrian-99@users.noreply.github.com>
2023-04-02 19:40:50 +0200

    Handling Java code execution (#1)
    * Handling Java code execution
    + added runSystemCommand method to AbstractExecutor
    # changed base image in Dockerfile to jdk variant
    + added CompilationFailedError to Error
    + added handling of CompilationFailedError to ErrorHandler
    + added JavaExecutor
    + added JAVA to Language

---
+++ kubexecutor/src/main/kotlin/pl/edu/prz/kod/executor/JavaExecutor.kt
@@ -0,0 +1,46 @@
+package pl.edu.prz.kod.executor
+
+import pl.edu.prz.kod.domain.ExecutionResult
+import pl.edu.prz.kod.domain.Language
+import pl.edu.prz.kod.error.CompilationFailedError
+import java.io.File
+import java.util.regex.Pattern
+
+class JavaExecutor: AbstractExecutor(Language.JAVA) {
+    private val srcDirPath = "/app/executor/src"
+    private val buildDirPath = "/app/executor/build"
+    private val runDirPath = "/app/executor/run"
+
+    override fun execute(code: String): ExecutionResult {
+        val srcDir = File(srcDirPath)
+        srcDir.mkdirs()
+        val runDir = File(runDirPath)
+        runDir.mkdirs()
+
+        val classNameMatcher = Pattern.compile("class\\s+([^\\s{]+)[\\s{]").matcher(code)
+        val javaClassName = if (classNameMatcher.find()) classNameMatcher.group(1) else "Main"
+
+        val packageNameMatcher = Pattern.compile("^\\s*package\\s+([^;]+);").matcher(code)
+        val javaPackageName =
+                if (packageNameMatcher.find())
+                    packageNameMatcher.group(1)
+                            .replace(Regex("\\s"), "") + '.'
+                else ""
+
+        val javaFile = srcDir.resolve("$javaClassName.java")
+        javaFile.createNewFile()
+        javaFile.writeText(code)
+
+        val compilationProcess = runSystemCommand("javac -d $buildDirPath ${javaFile.name}", srcDir)
+        if (compilationProcess.exitValue() != 0) {
+            throw CompilationFailedError(compilationProcess.errorStream.bufferedReader().readText())
+        }
+
+        val runProcess = runSystemCommand("java -classpath $buildDirPath $javaPackageName$javaClassName", runDir)
+        return ExecutionResult(
+                stdout = runProcess.inputStream.bufferedReader().readText(),
+                stdErr = runProcess.errorStream.bufferedReader().readText(),
+                exitCode = runProcess.exitValue()
+        )
+    }
+}
