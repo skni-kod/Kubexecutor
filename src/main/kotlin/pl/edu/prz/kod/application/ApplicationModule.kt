5a937e1a056e3afbe7f6ed991b700b0bfb63ac98
mherda64 <mherda64@gmail.com>
2023-04-13 11:47:46 +0200

    Fix missing KOIN dependency

--- src/main/kotlin/pl/edu/prz/kod/application/ApplicationModule.kt
+++ src/main/kotlin/pl/edu/prz/kod/application/ApplicationModule.kt
@@ -2,9 +2,10 @@ package pl.edu.prz.kod.application
 
 import org.koin.dsl.module
 import pl.edu.prz.kod.domain.executor.ExecutorOrchestrator
+import pl.edu.prz.kod.ports.ExecutorOrchestratorPort
 import java.util.Base64
 
 val applicationModule = module {
-    single { Base64.getDecoder() }
-    single { ExecutorOrchestrator() }
+    single<Base64.Decoder> { Base64.getDecoder() }
+    single<ExecutorOrchestratorPort> { ExecutorOrchestrator() }
 }
