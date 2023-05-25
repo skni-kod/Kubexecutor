0e38a34241825684bf8f48d06b1ee94f89039905
mherda64 <mherda64@gmail.com>
2023-05-25 15:13:04 +0200

    Fix default runner port

--- runner/src/main/kotlin/pl/edu/prz/kod/runner/application/Configuration.kt
+++ runner/src/main/kotlin/pl/edu/prz/kod/runner/application/Configuration.kt
@@ -3,6 +3,6 @@ package pl.edu.prz.kod.runner.application
 import pl.edu.prz.kod.common.application.BaseEnvironmentVariable
 
 data class Configuration(
-    val httpPort: Int = BaseEnvironmentVariable.getIntValue("HTTP_PORT", 8081),
+    val httpPort: Int = BaseEnvironmentVariable.getIntValue("HTTP_PORT", 8080),
     val systemCommandTimeout: Long = BaseEnvironmentVariable.getLongValue("SYSTEM_COMMAND_TIMEOUT", 5000)
 )
