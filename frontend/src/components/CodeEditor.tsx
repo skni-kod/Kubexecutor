050cf20c389e1831a1af631a56f347dc134929b4
Konrad Bochenek <56956303+Kazan1520@users.noreply.github.com>
2023-06-27 19:02:55 +0200

    remove unnecessary code

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {useEffect, useRef, useState} from "react";
+import React, {useRef, useState} from "react";
 import Editor, { Monaco } from "@monaco-editor/react";
 import type { editor as EditorType } from "monaco-editor";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
@@ -21,7 +21,7 @@ const CodeEditor = ({ authToken }: CodeEditorProps) => {
     const [monacoInstance, setMonacoInstance] = useState<Monaco | null>(null);
     const [output, setOutput] = useState<{ stdOut: string; stdErr: string; exitCode: number } | null>(null);
     const [error, setError] = useState<{ message: string } | null>(null);
-    const [currentText, setCurrentText] = useState(typeof window !== "undefined" ? localStorage.getItem("code") || '// Write your code here' : "// Write your code here");
+    const [currentText] = useState(typeof window !== "undefined" ? localStorage.getItem("code") || '// Write your code here' : "// Write your code here");
     const endpoint = "/api/execute"
 
     const getValue = async () => {
